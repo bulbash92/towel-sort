0c77cbd55ba04b8018082bf53348f243d93f3e0e
Evgeniy <49danon49@gmail.com>
2020-02-23 23:18:45 +0300

    fix: make task

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  if (matrix.length > 1) {
-    const arrTMP = [];
-
-    for (const item in matrix) {
-      if (item % 2 > 0) {
-        arrTMP.push(matrix[item].sort((a, b) => b > a ? 1 : -1));
-      }
-      else {
-        arrTMP.push(matrix[item].sort((a, b) => a > b ? 1 : -1));
-      }
-    }
-    return arrTMP.flat();
+  let arrTMP = [];
+  for (const item in matrix) {
+    if (item % 2 > 0) { arrTMP = arrTMP.concat(matrix[item].sort((a, b) => b > a ? 1 : -1)); }
+    else { arrTMP = arrTMP.concat(matrix[item].sort((a, b) => a > b ? 1 : -1)); }
   }
-  else { return matrix.flat().sort((a, b) => a > b ? 1 : -1); }
+  return arrTMP;
 
 }
