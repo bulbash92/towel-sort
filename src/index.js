e64914d1a56a9ae15a157ab96faaf9b7cdb570ed
Evgeniy <49danon49@gmail.com>
2020-02-23 23:01:56 +0300

    fear:solve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix.length > 1) {
+    const arrTMP = [];
+    const arrTwo = [];
+    for (const item in matrix) {
+      if (item % 2 > 0) {
+        arrTwo.push(matrix[item].sort((a, b) => b > a ? 1 : -1));
+      }
+      else {
+        arrTMP.push(matrix[item].sort((a, b) => a > b ? 1 : -1));
+      }
+    }
+    return arrTMP.concat(arrTwo);
+  }
+  else { return matrix.concat().sort((a, b) => a > b ? 1 : -1); }
+
 }
